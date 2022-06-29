cb35e3bea695ed056d2068f933b8df09667f52a8
Cyeon <77601035+Cyeon@users.noreply.github.com>
2022-06-29 18:26:03 +0900

    06/29 코드 수정

--- Assets/Scripts/Platform/FadePlatform.cs
+++ Assets/Scripts/Platform/FadePlatform.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class FadePlatform : PlatformBase
 {
     [SerializeField]
@@ -16,53 +16,26 @@ public class FadePlatform : PlatformBase
     }
     protected override void OnCollisionEnter2D(Collision2D collision)
     {
-        //base.OnCollisionEnter2D(collision);
         if (collision.transform.CompareTag("Player"))
         {
-            Debug.Log("COl");
-            StartCoroutine("Fade");
+            spriteRenderer.DOFade(0, 2);
+            Invoke("ColliderEnabled", 2f);
+        }
+        if (respawn)
+        {
+            Invoke("ColliderEnabled", 4f);
+            Invoke("SetRespawn", 4f);
         }
     }
 
     private void ColliderEnabled()
     {
         Collider2D col = GetComponent<Collider2D>();
-        if (col.enabled == true)
-        {
-            col.enabled = false;
-            return;
-        }
-        col.enabled = true;
+        col.enabled = !col.enabled;
     }
 
-    /// <summary>
-    /// 발판을 투명하게 합니다 
-    /// </summary>
-    /// <returns></returns>
-    IEnumerator Fade()
-    {
-        // for문 돌아가는 횟수랑 fadeAlpha 줄어드는 값/yeild return 값 수정해서 조금 더 부드럽게 움직이게 할 수 있음 
-        for (int i = 0; i < 10; i++)
-        {
-            fadeAlpha -= 0.1f;
-            spriteRenderer.color = new Color(1f, 1f, 1f, fadeAlpha);
-            yield return new WaitForSeconds(0.1f);
-        }
-        if (fadeAlpha <= 0)
-        {
-            ColliderEnabled();
-            if (respawn == true)
-            {
-                yield return new WaitForSeconds(4f);
-                ColliderEnabled();
-                SetRespawn();
-            }
-            StopCoroutine("Fade");
-        }
-    }
     void SetRespawn()
     {
         spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
-        fadeAlpha = 1f;
     }
 }
