cddd852e56c1d8c58adc6c03d5a82f360a2ec51e
LLL <77712364+elsam0104@users.noreply.github.com>
2022-08-04 00:30:41 +0900

    0802 궤적 UI가 점차 연해지도록 수정

--- Assets/02.Scripts/Throw/ThrowArm.cs
+++ Assets/02.Scripts/Throw/ThrowArm.cs
@@ -20,10 +20,16 @@ public class ThrowArm : MonoBehaviour
     private void Start()
     {
         points = new GameObject[numberOfPoints];
+        float alpa = 1f;
         for (int i = 0; i < numberOfPoints; i++)
         {
             points[i] = Instantiate(pointPrefab, fireTransform.position, fireTransform.rotation);
             points[i].transform.SetParent(PointsParentTransform);
+
+            SpriteRenderer renderer = points[i].GetComponent<SpriteRenderer>();
+            renderer.color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, alpa);
+            if (alpa > 0)
+                alpa -= 0.08f;
         }
     }
     private void Update()
@@ -41,13 +47,13 @@ public class ThrowArm : MonoBehaviour
     }
     private void FaceMouse()
     {
-        transform.right = dir; 
+        transform.right = dir;
     }
     private Vector2 PointPos(float t)
     {
         //P = p1 +vel*t+at^2/2
         //궤적 계산 공식
-        Vector2 currentPoints = (Vector2)fireTransform.position + dir.normalized * (force * t)+0.5f * Physics2D.gravity *(t*t);
+        Vector2 currentPoints = (Vector2)fireTransform.position + dir.normalized * (force * t) + 0.5f * Physics2D.gravity * (t * t);
         return currentPoints;
     }
 }
