25c5e7df9dbe4b942aeb0e3e7b21eb54156cd016
Cyeon <77601035+Cyeon@users.noreply.github.com>
2022-09-29 00:23:37 +0900

    09/28 코드 수정 중

--- Assets/02.Scripts/Platform/ExplosionPlatform.cs
+++ Assets/02.Scripts/Platform/ExplosionPlatform.cs
@@ -7,27 +7,30 @@ public class ExplosionPlatform : PlatformBase
     public float explosionPower = 0.5f;
     public float waitTime = 1.5f;
     public float colTime = 0f;
+    public bool isDamageOnce = false;
+    public Vector2 area = new Vector2(5, 5);
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        colTime = Time.time;
-        
+        StartCoroutine(Explosion());
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
+    private IEnumerator Explosion()
     {
-        if (colTime + waitTime <= Time.time)
+
+        yield return new WaitForSeconds(waitTime);
+
+        GameObject player = Physics2D.OverlapBox(transform.position, area, 0).gameObject;
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.position, area);
+        Gizmos.DrawSphere(transform.position, 5);
+        if (player != null)
         {
-            Debug.Log("call");
-            GameObject player = collision.gameObject;
-            Vector3 dir = player.transform.position - gameObject.transform.position;
-            dir = dir.normalized;
-            Explosion(player, dir);
+            player.GetComponent<Rigidbody2D>().AddForce(Vector2.up * explosionPower, ForceMode2D.Impulse);
             Damage();
-        }
-    }
 
-    private void Explosion(GameObject player, Vector3 dir)
-    {
-        player.GetComponent<Rigidbody2D>().AddForce(dir * explosionPower, ForceMode2D.Impulse);
+            isDamageOnce = true;
+            yield return new WaitForSeconds(3f);
+            isDamageOnce = false;
+        }
     }
 }
