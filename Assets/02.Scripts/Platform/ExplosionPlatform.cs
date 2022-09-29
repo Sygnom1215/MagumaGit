508c007d22a58117c34bbafc5c0618ff2cf34c41
Cyeon <77601035+Cyeon@users.noreply.github.com>
2022-09-29 19:29:48 +0900

    09/29 용암, 폭발하는 땅

--- Assets/02.Scripts/Platform/ExplosionPlatform.cs
+++ Assets/02.Scripts/Platform/ExplosionPlatform.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class ExplosionPlatform : PlatformBase
 {
-    public float explosionPower = 0.5f;
+    public float explosionPower = 20f;
     public float waitTime = 1.5f;
     public float colTime = 0f;
     public bool isDamageOnce = false;
-    public Vector2 area = new Vector2(5, 5);
+    public Vector2 area = new Vector2(2, 2);
+    public Vector2 positionModify = new Vector2(1, 0);
     private void OnCollisionEnter2D(Collision2D collision)
     {
         StartCoroutine(Explosion());
@@ -19,18 +20,25 @@ public class ExplosionPlatform : PlatformBase
 
         yield return new WaitForSeconds(waitTime);
 
-        GameObject player = Physics2D.OverlapBox(transform.position, area, 0).gameObject;
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireCube(transform.position, area);
-        Gizmos.DrawSphere(transform.position, 5);
-        if (player != null)
-        {
-            player.GetComponent<Rigidbody2D>().AddForce(Vector2.up * explosionPower, ForceMode2D.Impulse);
-            Damage();
+        Collider2D[] player = Physics2D.OverlapBoxAll(transform.position + (Vector3)positionModify, area, 0);
 
-            isDamageOnce = true;
-            yield return new WaitForSeconds(3f);
-            isDamageOnce = false;
+        foreach (Collider2D col in player)
+        {
+            if (col.CompareTag("Player"))
+            {
+                col.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * explosionPower, ForceMode2D.Impulse);
+                Damage();
+
+                isDamageOnce = true;
+                yield return new WaitForSeconds(3f);
+                isDamageOnce = false;
+            }
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.position + (Vector3)positionModify, area);
     }
 }
